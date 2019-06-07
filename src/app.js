2a202ebdc6cd128f0796e0f99ca821068e3b6141
Daniel Gillespie <daniel.gillespie@pluto.tv>
2019-06-07 14:40:07 -0700

    added more tests. updated test results default config.

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ class DatabaseTest {
     static #payloadData = [];
     static #postgres = {};
     static #mongodb = {};
+    static #maxRecords = 0;
 
     static async init() {
         this.loadConfig();
@@ -36,6 +37,7 @@ class DatabaseTest {
             this.#payloadData.push(fs.readFileSync(this.#config.DATA_DIR + file));
         }
 
+        this.#maxRecords = this.#config.DATA_INSERT_LOOP_COUNT * (this.#payloadData.length - 1);
         console.log(`Loaded ${this.#payloadData.length} file(s) with ${(sizeof(this.#payloadData) / 1000000).toFixed(2)} megabytes of data!`);
     }
 
@@ -80,7 +82,9 @@ class DatabaseTest {
                 insertFullBlob: [],
                 insertFullBlobAverage: 0,
                 partialBlobRead: [],
+                partialBlobReadAverage: 0,
                 partialBlobWrite: [],
+                partialBlobWriteAverage: 0,
                 fullBlobRead: [],
                 fullBlobReadAverage: 0,
             },
@@ -88,21 +92,25 @@ class DatabaseTest {
                 insertFullBlob: [],
                 insertFullBlobAverage: 0,
                 partialBlobRead: [],
+                partialBlobReadAverage: 0,
                 partialBlobWrite: [],
+                partialBlobWriteAverage: 0,
                 fullBlobRead: [],
                 fullBlobReadAverage: 0,
             }
         };
 
         await this.insertJsonBlobTestPostgres();
-        await this.readJsonFullBlobTestProgress();
+        await this.readJsonFullBlobTestPostgres();
+        await this.writeJsonPartialBlobTestPostgres();
+        await this.readJsonPartialBlobTestPostgres();
     }
 
     static async insertJsonBlobTestPostgres() {
         console.log(`Inserting ${this.#config.DATA_INSERT_LOOP_COUNT * this.#payloadData.length} test items into two test tables in database!`);
 
         for (let i = 0; i < this.#config.DATA_INSERT_LOOP_COUNT; i++) {
-            console.log(`Data insert loop ${i} of ${this.#config.DATA_INSERT_LOOP_COUNT} executing...`);
+            console.log(`Data insert loop ${i+1} of ${this.#config.DATA_INSERT_LOOP_COUNT} executing...`);
             for (const item of this.#payloadData) {
                 const id = uuid.v4();
 
@@ -127,20 +135,19 @@ class DatabaseTest {
         console.log(`INSERT FULL BLOB JSONB AVERAGE: ${this.#postgres.results.jsonb.insertFullBlobAverage}ms`);
     }
 
-    static async readJsonFullBlobTestProgress() {
+    static async readJsonFullBlobTestPostgres() {
         console.log('Starting Postgres Read Full JSON blob from row test...!');
-        const maxRecords = (this.#config.DATA_INSERT_LOOP_COUNT * this.#payloadData.length) - 1;
 
         for (let i = 0; i < this.#config.TEST_ITERATION_COUNT; i++) {
-            console.log(`Executing test iteration ${i} of ${this.#config.TEST_ITERATION_COUNT}`);
+            console.log(`Executing test iteration ${i+1} of ${this.#config.TEST_ITERATION_COUNT}`);
 
             const startTime1 = new Date();
-            const result1 = await this.#postgres.pool.query(`SELECT value FROM test_json WHERE id = ${this.generateRandomNumber(0, maxRecords)}`);
+            const result1 = await this.#postgres.pool.query(`SELECT value FROM test_json WHERE id = ${this.generateRandomNumber(1, this.#maxRecords)}`);
             const endTime1 = new Date();
             this.#postgres.results.json.fullBlobRead.push(endTime1 - startTime1);
 
             const startTime2 = new Date();
-            const result2 = await this.#postgres.pool.query(`SELECT value FROM test_jsonb WHERE id = ${this.generateRandomNumber(0, maxRecords)}`);
+            const result2 = await this.#postgres.pool.query(`SELECT value FROM test_jsonb WHERE id = ${this.generateRandomNumber(1, this.#maxRecords)}`);
             const endTime2 = new Date();
             this.#postgres.results.jsonb.fullBlobRead.push(endTime2 - startTime2);
         }
@@ -152,6 +159,70 @@ class DatabaseTest {
 
         this.#postgres.results.jsonb.fullBlobReadAverage = this.average(this.#postgres.results.jsonb.fullBlobRead);
         console.log(`READ FULL BLOB JSONB AVERAGE: ${this.#postgres.results.jsonb.fullBlobReadAverage}ms`);
+    }
+
+    static async writeJsonPartialBlobTestPostgres() {
+        console.log('Starting Postgres Write Partial JSON blob from row test...!');
+
+        for (let i = 0; i < this.#config.TEST_ITERATION_COUNT; i++) {
+            console.log(`Executing test iteration ${i+1} of ${this.#config.TEST_ITERATION_COUNT}`);
+
+            // const random1 = this.generateRandomNumber(1, this.#maxRecords);
+            // const result1 = await this.#postgres.pool.query(`SELECT value FROM test_json WHERE id = ${random1}`);
+            // const blob1 = result1.rows[0].value;
+            // const startTime1 = new Date();
+            // await this.#postgres.pool.query(`SELECT value->'${this.getRandomPropertyName(blob1)}' FROM test_json WHERE id = ${random1}`);
+            // const endTime1 = new Date();
+            // this.#postgres.results.json.partialBlobWrite.push(endTime1 - startTime1);
+
+            const random2 = this.generateRandomNumber(1, this.#maxRecords);
+            const result2 = await this.#postgres.pool.query(`SELECT value FROM test_jsonb WHERE id = ${random2}`);
+            const blob2 = result2.rows[0].value;
+            const startTime2 = new Date();
+            await this.#postgres.pool.query(`UPDATE test_jsonb SET value = jsonb_set(value, '{${this.getRandomPropertyName(blob2)}}'::text[], '"test-value-here"'::jsonb, true) WHERE id = ${random2}`);
+            const endTime2 = new Date();
+            this.#postgres.results.jsonb.partialBlobWrite.push(endTime2 - startTime2);
+        }
+
+        console.log('Write Partial Json Blob Test Complete!');
+        console.log('Results:');
+        this.#postgres.results.json.partialBlobWriteAverage = this.average(this.#postgres.results.json.partialBlobWrite);
+        console.log(`WRITE PARTIAL BLOB JSON AVERAGE: ${this.#postgres.results.json.partialBlobWriteAverage}ms`);
+
+        this.#postgres.results.jsonb.partialBlobWriteAverage = this.average(this.#postgres.results.jsonb.partialBlobWrite);
+        console.log(`WRITE PARTIAL BLOB JSONB AVERAGE: ${this.#postgres.results.jsonb.partialBlobWriteAverage}ms`);
+    }
+
+    static async readJsonPartialBlobTestPostgres() {
+        console.log('Starting Postgres Read Partial JSON blob from row test...!');
+
+        for (let i = 0; i < this.#config.TEST_ITERATION_COUNT; i++) {
+            console.log(`Executing test iteration ${i+1} of ${this.#config.TEST_ITERATION_COUNT}`);
+
+            const random1 = this.generateRandomNumber(1, this.#maxRecords);
+            const result1 = await this.#postgres.pool.query(`SELECT value FROM test_json WHERE id = ${random1}`);
+            const blob1 = result1.rows[0].value;
+            const startTime1 = new Date();
+            await this.#postgres.pool.query(`SELECT value->'${this.getRandomPropertyName(blob1)}' FROM test_json WHERE id = ${random1}`);
+            const endTime1 = new Date();
+            this.#postgres.results.json.partialBlobRead.push(endTime1 - startTime1);
+
+            const random2 = this.generateRandomNumber(1, this.#maxRecords);
+            const result2 = await this.#postgres.pool.query(`SELECT value FROM test_jsonb WHERE id = ${random2}`);
+            const blob2 = result2.rows[0].value;
+            const startTime2 = new Date();
+            await this.#postgres.pool.query(`SELECT value->'${this.getRandomPropertyName(blob2)}' FROM test_jsonb WHERE id = ${random2}`);
+            const endTime2 = new Date();
+            this.#postgres.results.jsonb.partialBlobRead.push(endTime2 - startTime2);
+        }
+
+        console.log('Read Partial Json Blob Test Complete!');
+        console.log('Results:');
+        this.#postgres.results.json.partialBlobReadAverage = this.average(this.#postgres.results.json.partialBlobRead);
+        console.log(`READ PARTIAL BLOB JSON AVERAGE: ${this.#postgres.results.json.partialBlobReadAverage}ms`);
+
+        this.#postgres.results.jsonb.partialBlobReadAverage = this.average(this.#postgres.results.jsonb.partialBlobRead);
+        console.log(`READ PARTIAL BLOB JSONB AVERAGE: ${this.#postgres.results.jsonb.partialBlobReadAverage}ms`);
     }
 
     static setupMongoDatabase() {
@@ -169,8 +240,13 @@ class DatabaseTest {
     }
 
     static generateRandomNumber(min, max) {
-        return Math.random() * (max - min) + min;
+        return Math.floor((Math.random() * (max - min)) + min);
     }
+
+    static getRandomPropertyName(obj) {
+        const keys = Object.keys(obj);
+        return keys[keys.length * Math.random() << 0];
+    };
 }
 
 DatabaseTest.init();
